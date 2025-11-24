c84ed431945a439cabbfe9dd5e8e82da57096c6f
iararaquel <yararaquel2020@gmail.com>
2025-11-24 14:44:53 -0300

    feat: Adicionando enumerate para os estados emocionais

---
+++ src/br/com/tamagotchi/enums/Humor.java
@@ -0,0 +1,13 @@
+package br.com.tamagotchi.enums;
+
+/**
+ Aqui defiinimos os estados emocionais do possiveis digimons.
+ */
+
+public enum Humor {
+    FELIZ,
+    TRISTE,
+    COM_FOME,
+    CANSADO,
+    MORTO
+}
