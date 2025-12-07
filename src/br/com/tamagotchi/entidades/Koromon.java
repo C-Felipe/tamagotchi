1d24061e2a7336bebf5febf6e87c7cb30a8446af
joaocarlos <joao.rocha53396@alunos.ufersa.edu.br>
2025-12-07 16:47:12 -0300

    uptade Agumon e Koromon

--- src/br/com/tamagotchi/entidades/Koromon.java
+++ src/br/com/tamagotchi/entidades/Koromon.java
@@ -1,44 +1,13 @@
-package br.com.tamagotchi.entidades;
+koromon.java
 
-import br.com.tamagotchi.enums.Humor;
+package br.com.tamagotchi.entidades;
 
 public class Koromon extends Tamagotchi {
 
     public Koromon() {
         super("Koromon");
+        // Bebês começam com status diferentes
         setEnergia(80);
         setFome(20);
-    }
-    
-@Override
-    public void alimentar() {
-        System.out.println(getNome() + " mordiscou um pedacinho de comida.");
-        setFome(Math.max(0, getFome() - 5));
-        ganharExperiencia(10);
-    }
-
-    @Override
-    public void brincar() {
-        System.out.println(getNome() + " soltou bolhas de sabão!");
-        setFelicidade(Math.min(100, getFelicidade() + 10));
-        setEnergia(getEnergia() - 5);
-        setFome(getFome() + 5);
-        ganharExperiencia(15);
-    }
-
-    @Override
-    public void dormir() {
-        System.out.println(getNome() + " fechou os olhinhos e dormiu.");
-        setEnergia(100);
-        setFome(getFome() + 2);
-    }
-
-    @Override
-    public void atualizarHumor() {
-        if (getFome() > 50 || getEnergia() < 40) {
-            setHumor(Humor.TRISTE);
-        } else {
-            setHumor(Humor.FELIZ);
-        }
     }
 }
