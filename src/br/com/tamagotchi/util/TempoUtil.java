a6ddd5b4c06d7cb28d8871384a398292d6c4154d
Igor <igorcavalcanterocha2016@gmail.com>
2025-12-07 21:11:18 -0300

    atualizando classe TempoUtil

--- src/br/com/tamagotchi/util/TempoUtil.java
+++ src/br/com/tamagotchi/util/TempoUtil.java
@@ -5,60 +5,41 @@ import br.com.tamagotchi.enums.Humor;
 import java.util.Timer;
 import java.util.TimerTask;
 
-/**
- * Utilitário responsável por controlar o relógio do jogo.
- * Executa ciclos automáticos de atualização (game loop).
- */
 public class TempoUtil {
 
     private Timer timer;
     private TamagotchiService service;
-    private boolean rodando;
 
-    /** Configuração de velocidade (em milissegundos)
-     * 1000ms = 1 segundo na vida real
-     * Quanto menor, mais rápido o jogo passa
-     */
-    private static final int INTERVALO_ATUALIZACAO = 2000;
+    // Atualiza o jogo a cada 5 segundos
+    private static final int INTERVALO = 5000;
 
     public TempoUtil(TamagotchiService service) {
         this.service = service;
     }
 
-    /**
-     * Inicia a contagem do tempo.
-     * O jogo começa a "rodar" sozinho a partir daqui.
-     */
     public void iniciar() {
-        if (timer != null ) return; //Evita criar dois timers
+        if (timer != null) return;
 
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                //chama o serviço automaticamente
-                service.processarPassagemDeTempo();
+                if (service.getTamagotchi() != null) {
+                    service.processarPassagemDeTempo();
 
-                // Se o digimon morreu, o relogio para
-                if (service.getTamagotchi().getHumor() == Humor.MORTO) {
-                    parar();
+                    // Se morrer, para o loop para economizar recursos
+                    if (service.getTamagotchi().getHumor() == Humor.MORTO) {
+                        parar();
+                    }
                 }
-
-                // (Futuramente aqui chamaremos a atualização da Janela também)
-                // System.out.println("Tick... (Tempo passando)"); // Debug
             }
-        }, 0, INTERVALO_ATUALIZACAO);
+        }, 0, INTERVALO);
     }
 
-    /**
-     * Pausa o tempo (útil para menus ou pause).
-     */
     public void parar() {
         if (timer != null) {
             timer.cancel();
             timer = null;
         }
-        rodando = false;
-        System.out.println("Tempo pausado.");
     }
 }
