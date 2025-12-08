26477379ef900d5e88f8bf8db9db7fb46bef69dd
c-felipe <felipeti.profissional@gmail.com>
2025-12-08 17:49:28 -0300

    atualizando PersistenciaService

--- src/br/com/tamagotchi/servicos/PersistenciaService.java
+++ src/br/com/tamagotchi/servicos/PersistenciaService.java
@@ -1,20 +1,18 @@
 package br.com.tamagotchi.servicos;
 
 import br.com.tamagotchi.entidades.Tamagotchi;
-import br.com.tamagotchi.enums.Humor;
-
 import java.io.*;
 
 public class PersistenciaService {
+
     private static final String NOME_ARQUIVO = "savegame.dat";
 
     public void salvar(Tamagotchi tamagotchi) {
-        // Para tentar criar um fluxo de saida de para o arquivo;
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(NOME_ARQUIVO))) {
             oos.writeObject(tamagotchi);
-            System.out.println("Jogo salvo com sucesso em: " + NOME_ARQUIVO);
+            System.out.println("Jogo salvo em: " + NOME_ARQUIVO);
         } catch (IOException e) {
-            System.out.println("Erro ao salvar o jogo: " + e.getMessage());
+            System.out.println("Erro ao salvar: " + e.getMessage());
         }
     }
 
@@ -22,14 +20,13 @@ public class PersistenciaService {
         File arquivo = new File(NOME_ARQUIVO);
 
         if (!arquivo.exists()) {
-            return null;  //Retorna nulo se não existir arquivo de save anterior;
+            return null;
         }
 
-        //Para tentar ler o objeto do arquivo;
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(arquivo))) {
             return (Tamagotchi) ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Erro ao carregar o jogo: " + e.getMessage());
+            System.out.println("Erro ao carregar save (arquivo corrompido ou versão antiga).");
             return null;
         }
     }
@@ -38,7 +35,7 @@ public class PersistenciaService {
         File arquivo = new File(NOME_ARQUIVO);
         if (arquivo.exists()) {
             arquivo.delete();
+            System.out.println("Save anterior deletado.");
         }
     }
-
 }
